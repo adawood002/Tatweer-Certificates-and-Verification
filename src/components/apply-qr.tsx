1a0746080061c098984d896673a491b648d84725
Synapse Projects <synapseaiprojects@gmail.com>
2025-07-30 03:09:10 +0000

    The expiry date picker should be able to pick older dates too

--- src/components/apply-qr.tsx
+++ src/components/apply-qr.tsx
@@ -505,7 +505,6 @@ function ApplyQrCode() {
                           mode="single"
                           selected={field.value}
                           onSelect={field.onChange}
-                          disabled={(date) => date < new Date()}
                           initialFocus
                         />
                       </PopoverContent>
